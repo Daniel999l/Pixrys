db2ca1c241edd003d6d16d44a39fda8efa134c58
Daniel999l <danielishola710@gmail.com>
2025-09-24 01:17:12 +0100

    Update script.js

--- Auth/script.js
+++ Auth/script.js
@@ -198,32 +198,3 @@ document.addEventListener('DOMContentLoaded', () => {
 document.getElementById("error_alrt").addEventListener("click", function () {
     alert("Due to safety reasons, signup via email is disabled for now. Please use Google or Twitter to sign in.");
   });
-
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-  import { getAuth, TwitterAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
-
-  // Your Firebase config
-  const firebaseConfig = {
-    apiKey: "AIzaSyCqVGh1TeRg9a-tWzhUiGudMb-vX24znT0",
-    authDomain: "twitter-login-demo-3ef40.firebaseapp.com",
-    projectId: "twitter-login-demo-3ef40",
-    storageBucket: "twitter-login-demo-3ef40.firebasestorage.app",
-    messagingSenderId: "597647210178",
-    appId: "1:597647210178:web:7e0328234ac0deceeb2d5b",
-    measurementId: "G-CTT5P63LLY"
-  };
-
-  // Init Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-
-  // Twitter login
-  window.twitterLogin = async function() {
-    const provider = new TwitterAuthProvider();
-    try {
-      await signInWithPopup(auth, provider);
-      window.location.href = "../Main/index.html"; // redirect after login
-    } catch (error) {
-      console.error("Twitter login failed:", error);
-    }
-  }
