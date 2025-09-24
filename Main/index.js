166644242fb34c02db1c31f17fc14fb4b946f1b3
Daniel999l <danielishola710@gmail.com>
2025-09-24 01:28:27 +0100

    Update index.js

--- Main/index.js
+++ Main/index.js
@@ -5,37 +5,36 @@
     icon.classList.toggle("text-danger"); // red color
   }
 
-   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-  import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 
-  // Your Firebase config
-  const firebaseConfig = {
-    apiKey: "AIzaSyCqVGh1TeRg9a-tWzhUiGudMb-vX24znT0",
-    authDomain: "twitter-login-demo-3ef40.firebaseapp.com",
-    projectId: "twitter-login-demo-3ef40",
-    storageBucket: "twitter-login-demo-3ef40.firebasestorage.app",
-    messagingSenderId: "597647210178",
-    appId: "1:597647210178:web:7e0328234ac0deceeb2d5b",
-    measurementId: "G-CTT5P63LLY"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCqVGh1TeRg9a-tWzhUiGudMb-vX24znT0",
+  authDomain: "twitter-login-demo-3ef40.firebaseapp.com",
+  projectId: "twitter-login-demo-3ef40",
+  storageBucket: "twitter-login-demo-3ef40.firebasestorage.app",
+  messagingSenderId: "597647210178",
+  appId: "1:597647210178:web:7e0328234ac0deceeb2d5b",
+  measurementId: "G-CTT5P63LLY"
+};
 
-  // Init Firebase
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-  // Check user state
-  onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, (user) => {
   if (user) {
-    // Twitter display name
-    document.getElementById("username").textContent = "@" + user.reloadUserInfo.screenName;
+    // Set PFP
+    const pfpEl = document.getElementById("pfp");
+    if (pfpEl) pfpEl.src = user.photoURL;
 
-    // Twitter profile picture
-    document.getElementById("pfp").src = user.photoURL;
+    // Set username
+    const usernameEl = document.getElementById("username");
+    if (usernameEl) usernameEl.textContent = "@" + user.reloadUserInfo.screenName;
 
-    // Twitter profile link
-    document.getElementById("twitterProfile").href = "https://x.com/" + user.reloadUserInfo.screenName;
+    // Set link to X profile
+    const profileLink = document.getElementById("twitterProfile");
+    if (profileLink) profileLink.href = "https://x.com/" + user.reloadUserInfo.screenName;
   } else {
-    // Kick them back to login
-    window.location.href = "../Auth/index.html";
+    window.location.href = "../Auth/index.html"; // Not logged in
   }
 });
